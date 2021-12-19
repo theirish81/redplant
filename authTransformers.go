621afa1b7cc8d9fae1f8394fcc5df0a46cbe1a08
theirish81 <theirish81@gmail.com>
2021-12-19 18:29:07 +0100

    single config structure for JWTAuthTransformer

--- authTransformers.go
+++ authTransformers.go
@@ -11,88 +11,88 @@ import (
 // BasicAuthTransformer is a transformer that will block the request in case the credentials do not match the
 // expectations
 type BasicAuthTransformer struct {
-	Username	string	`mapstructure:"username"`
-	Password	string	`mapstructure:"password"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
 }
 
 // Transform will throw an error if the request doesn't match the basic auth expectations
-func (t *BasicAuthTransformer) Transform(wrapper *APIWrapper) (*APIWrapper,error) {
-	username,password,ok := wrapper.Request.BasicAuth()
-	if ok && t.Username == username && t.Password==password {
+func (t *BasicAuthTransformer) Transform(wrapper *APIWrapper) (*APIWrapper, error) {
+	username, password, ok := wrapper.Request.BasicAuth()
+	if ok && t.Username == username && t.Password == password {
 		wrapper.Username = t.Username
-		return wrapper,nil
+		return wrapper, nil
 	} else {
-		return nil,errors.New("no_auth")
+		return nil, errors.New("no_auth")
 	}
 }
+
 // NewBasicAuthTransformer creates a BasicAuthTransformer from params
-func NewBasicAuthTransformer(params map[string]interface{}) (*BasicAuthTransformer,error) {
+func NewBasicAuthTransformer(params map[string]interface{}) (*BasicAuthTransformer, error) {
 	var t BasicAuthTransformer
 	//err := mapstructure.Decode(params,&t)
-	err := DecodeAndTempl(params,&t,nil)
-	return &t,err
+	err := DecodeAndTempl(params, &t, nil)
+	return &t, err
 }
 
 // JWTAuthTransformer will block any request without a Bearer token or a token whose signature cannot be verified.
 // In addition, it will store claims in the wrapper.
 type JWTAuthTransformer struct {
-	publicKey *rsa.PublicKey
-	key		  []byte
-}
-
-type JWTAuthConfig struct {
-	Pem	string	`mapstructure:"pem"`
-	Key	string	`mapstructure:"key"`
+	_publicKey *rsa.PublicKey
+	_key       []byte
+	Pem        string `mapstructure:"pem"`
+	Key        string `mapstructure:"key"`
 }
 
 // Transform will block any request without a Bearer token or a token whose signature cannot be verified.
 // In addition, it will store claims in the wrapper.
-func (t *JWTAuthTransformer) Transform(wrapper *APIWrapper) (*APIWrapper,error) {
+func (t *JWTAuthTransformer) Transform(wrapper *APIWrapper) (*APIWrapper, error) {
 	header := wrapper.Request.Header.Get("authorization")
 
 	// if no Bearer prefix, then we have no auth
-	if !strings.HasPrefix(header,"Bearer") {
-		return nil,errors.New("no_auth")
+	if !strings.HasPrefix(header, "Bearer") {
+		return nil, errors.New("no_auth")
 	}
 
 	// Extract the token from the header
 	token := strings.TrimSpace(header[7:])
 	claims := jwt.MapClaims{}
 	// Parsing with signature check and claims extraction
-	_,err := jwt.ParseWithClaims(token, &claims, func(token *jwt.Token) (interface{}, error) {
-		// If we have a public key, we'll use that
-		if t.publicKey != nil {
-			return t.publicKey,nil
+	_, err := jwt.ParseWithClaims(token, &claims, func(token *jwt.Token) (interface{}, error) {
+		// If we have a public _key, we'll use that
+		if t._publicKey != nil {
+			return t._publicKey, nil
 		}
-		// Otherwise, we'll just use the key
-		return t.key,nil
+		// Otherwise, we'll just use the _key
+		return t._key, nil
 	})
 	// Storing claims in the wrapper
 	wrapper.Claims = &claims
-	return wrapper,err
+	return wrapper, err
 }
 
 // NewJWTAuthTransformer creates a new JWTAuthTransformer from params
-func NewJWTAuthTransformer(params map[string]interface{}) (*JWTAuthTransformer,error) {
-	var cfg JWTAuthConfig
-	err := DecodeAndTempl(params,&cfg,nil)
+func NewJWTAuthTransformer(params map[string]interface{}) (*JWTAuthTransformer, error) {
+	t := JWTAuthTransformer{}
+	err := DecodeAndTempl(params, &t, nil)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	// If there's a "pem" parameter, then we treat that as a path to the certificate
-	if cfg.Pem!="" {
-		cert,err :=ioutil.ReadFile(cfg.Pem)
+	if t.Pem != "" {
+		cert, err := ioutil.ReadFile(t.Pem)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
-		key,err := jwt.ParseRSAPublicKeyFromPEM(cert)
-		return &JWTAuthTransformer{publicKey: key},err
+		key, err := jwt.ParseRSAPublicKeyFromPEM(cert)
+		t._publicKey = key
+		return &t, err
 	}
-	// If there's a "key" parameter, we expect the string to be the key itself
-	if cfg.Key!="" {
-		byteKey := []byte(cfg.Key)
-		return &JWTAuthTransformer{key: byteKey},nil
+	// If there's a "_key" parameter, we expect the string to be the _key itself
+	if t.Key != "" {
+		byteKey := []byte(t.Key)
+		t._key = byteKey
+		return &t, nil
 	}
 	// If none of the above is verified, then the configuration is missing and we can error out
-	return nil,errors.New("jwt_auth_transformer_no_config")
+	return nil, errors.New("jwt_auth_transformer_no_config")
 }
