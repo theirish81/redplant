ca8b47ca196dbc6e75cb92ae4aeb69fd4fbcaad8
theirish81 <theirish81@gmail.com>
2021-12-28 15:30:59 +0100

    slight code simplification. Documentation.

--- transformers.go
+++ transformers.go
@@ -2,8 +2,7 @@ package main
 
 import "net/http"
 
-// *** REQUEST TRANSFORMERS ***
-
+// IRequestTransformer is the interface for all request transformers
 type IRequestTransformer interface {
 	Transform(req *APIWrapper) (*APIWrapper, error)
 	ShouldExpandRequest() bool
@@ -11,24 +10,23 @@ type IRequestTransformer interface {
 	HandleError(writer *http.ResponseWriter)
 }
 
+// RequestTransformers is the store for all request transformers, associated to a given route
 type RequestTransformers struct {
 	transformers []IRequestTransformer
 }
 
+// ShouldExpandRequest will return true if at least one transformer requires the request to be expanded
 func (t *RequestTransformers) ShouldExpandRequest() bool {
-	expand := false
 	for _, tx := range t.transformers {
 		if tx.ShouldExpandRequest() {
-			expand = true
+			return true
 		}
 	}
-	return expand
+	return false
 }
 
+// Transform will process all request transformers for the given wrapper
 func (t *RequestTransformers) Transform(wrapper *APIWrapper) (*APIWrapper, error) {
-	defer func() {
-
-	}()
 	for _, transformer := range t.transformers {
 		if _, err := transformer.Transform(wrapper); err != nil {
 			return wrapper, err
@@ -37,10 +35,13 @@ func (t *RequestTransformers) Transform(wrapper *APIWrapper) (*APIWrapper, error
 	return wrapper, nil
 }
 
+// Push will append a transformer to the transformers
 func (t *RequestTransformers) Push(transformer IRequestTransformer) {
 	t.transformers = append(t.transformers, transformer)
 }
 
+// FindErrorHandler will find a transformer that has the capability of handling a certain error.
+// if the transformer is not found, then nil is returned
 func (t *RequestTransformers) FindErrorHandler(err error) IRequestTransformer {
 	for _, tx := range t.transformers {
 		if tx.ErrorMatches(err) {
@@ -50,6 +51,8 @@ func (t *RequestTransformers) FindErrorHandler(err error) IRequestTransformer {
 	return nil
 }
 
+// HandleError will try to delegate a error to a transformer capable of handling it. If a suitable transformer is found
+// then true is returned. False otherwise.
 func (t *RequestTransformers) HandleError(err error, writer *http.ResponseWriter) bool {
 	handler := t.FindErrorHandler(err)
 	if handler == nil {
@@ -60,6 +63,7 @@ func (t *RequestTransformers) HandleError(err error, writer *http.ResponseWriter
 	}
 }
 
+// NewRequestTransformers initializes all request transformers, based on their configurations
 func NewRequestTransformers(transformers *[]TransformerConfig) (*RequestTransformers, error) {
 	res := RequestTransformers{}
 	for _, t := range *transformers {
@@ -114,8 +118,7 @@ func NewRequestTransformers(transformers *[]TransformerConfig) (*RequestTransfor
 	return &res, nil
 }
 
-// *** RESPONSE TRANSFORMERS ***
-
+// IResponseTransformer is the interface for all response transformers
 type IResponseTransformer interface {
 	Transform(wrapper *APIWrapper) (*APIWrapper, error)
 	ShouldExpandRequest() bool
@@ -124,30 +127,32 @@ type IResponseTransformer interface {
 	HandleError(writer *http.ResponseWriter)
 }
 
+// ResponseTransformers is the store for the response transformers for a given route
 type ResponseTransformers struct {
 	transformers []IResponseTransformer
 }
 
+// ShouldExpandRequest will return true if at least one transformer needs the request to be expanded
 func (t *ResponseTransformers) ShouldExpandRequest() bool {
-	expand := false
 	for _, tx := range t.transformers {
 		if tx.ShouldExpandRequest() {
-			expand = true
+			return true
 		}
 	}
-	return expand
+	return false
 }
 
+// ShouldExpandResponse will return true if at least one transformer needs the response to be expanded
 func (t *ResponseTransformers) ShouldExpandResponse() bool {
-	expand := false
 	for _, tx := range t.transformers {
 		if tx.ShouldExpandResponse() {
-			expand = true
+			return true
 		}
 	}
-	return expand
+	return false
 }
 
+// Transform will process the whole response transformation pipeline
 func (t *ResponseTransformers) Transform(wrapper *APIWrapper) (*APIWrapper, error) {
 	for _, transformer := range t.transformers {
 		if wrapper, err := transformer.Transform(wrapper); err != nil {
@@ -157,10 +162,13 @@ func (t *ResponseTransformers) Transform(wrapper *APIWrapper) (*APIWrapper, erro
 	return wrapper, nil
 }
 
+// Push will append a transformer to the response transformers
 func (t *ResponseTransformers) Push(transformer IResponseTransformer) {
 	t.transformers = append(t.transformers, transformer)
 }
 
+// FindErrorHandler will find a transformer that has the capability of handling a certain error.
+// if the transformer is not found, then nil is returned
 func (t *ResponseTransformers) FindErrorHandler(err error) IRequestTransformer {
 	for _, tx := range t.transformers {
 		if tx.ErrorMatches(err) {
@@ -170,6 +178,8 @@ func (t *ResponseTransformers) FindErrorHandler(err error) IRequestTransformer {
 	return nil
 }
 
+// HandleError will try to delegate a error to a transformer capable of handling it. If a suitable transformer is found
+// then true is returned. False otherwise.
 func (t *ResponseTransformers) HandleError(err error, writer *http.ResponseWriter) bool {
 	handler := t.FindErrorHandler(err)
 	if handler == nil {
@@ -180,6 +190,7 @@ func (t *ResponseTransformers) HandleError(err error, writer *http.ResponseWrite
 	}
 }
 
+// NewResponseTransformers initializes all response transformers, based on their configurations
 func NewResponseTransformers(transformers *[]TransformerConfig) (*ResponseTransformers, error) {
 	res := ResponseTransformers{}
 	for _, t := range *transformers {
