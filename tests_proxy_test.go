332e5a7da35195fe2af6016a88abafcf5cd1a986
theirish81 <theirish81@gmail.com>
2021-12-31 12:23:13 +0100

    tripper and proxy tests

---
+++ tests_proxy_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestSetupRouter(t *testing.T) {
+	log = NewLogHelper("", logrus.InfoLevel)
+	config = LoadConfig("test_data/test_config.yaml")
+	config.Init()
+	router := SetupRouter()
+	route := router.GetRoute("localhost:9001")
+	if route == nil {
+		t.Error("Route not properly registered")
+	}
+}
+func TestReverseProxy(t *testing.T) {
+	log = NewLogHelper("", logrus.InfoLevel)
+	config = LoadConfig("test_data/test_config.yaml")
+	config.Init()
+	router := SetupRouter()
+	go func() {
+		_ = http.ListenAndServe(":"+strconv.Itoa(config.Network.Downstream.Port), router)
+	}()
+
+	d, _ := time.ParseDuration("2s")
+	time.Sleep(d)
+	res, _ := http.Get("http://foo:bar@localhost:9001/todo/1")
+	if !strings.Contains(res.Header.Get("Content-Type"), "application/json") {
+		t.Error("Something went wrong with dry run")
+	}
+}
---
+++ tests_tripper_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestRoundTripperFilter_RoundTrip(t *testing.T) {
+	config = Config{}
+	config.Network.Upstream.Timeout = "10s"
+	config.Network.Upstream.KeepAlive = "5s"
+	config.Network.Upstream.IdleConnectionTimeout = "2s"
+	config.Network.Upstream.ExpectContinueTimeout = "10s"
+	transport := configTransport()
+	request := &http.Request{Header: http.Header{}}
+	request.URL, _ = url.Parse("https://www.google.com")
+	request = ReqWithContext(request, nil)
+	GetWrapper(request).Request = request
+	response, _ := transport.RoundTrip(request)
+	if response == nil {
+		t.Error("Could not roundtrip to response")
+	}
+	if response.Header.Get("Content-Type") == "" {
+		t.Error("Response seems invalid")
+	}
+}
