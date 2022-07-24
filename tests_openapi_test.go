205b8fd6d731a5b19db4e6a4da5a117662fb5eb9
theirish81 <theirish81@gmail.com>
2022-07-24 14:38:18 +0200

    petstore openapi, test [skip ci]

---
+++ tests_openapi_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestOpenAPI2Rules(t *testing.T) {
+	log = NewLogHelper("", logrus.InfoLevel)
+	cfg := &OpenAPIConfig{File: "etc/openapi.yaml"}
+	rules := OpenAPI2Rules(map[string]*OpenAPIConfig{"localhost": cfg, "127.0.0.1": cfg})
+	if _, ok := rules["localhost"]; !ok {
+		t.Error("did not properly map domain")
+	}
+	if _, ok := rules["127.0.0.1"]; !ok {
+		t.Error("did not properly map domain")
+	}
+	if _, ok := rules["localhost"]["[get] /v1/pets/.*"]; !ok {
+		t.Error("could not find path")
+	}
+}
