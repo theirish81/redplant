07c4f9851e49a4d1cbb4ae2d59c82e022de1e505
theirish81 <theirish81@gmail.com>
2022-08-20 15:34:40 +0200

    tests adjusted

--- tests_transformers_barrage_test.go
+++ tests_transformers_barrage_test.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
 func TestBarrageRequestTransformer_Transform(t *testing.T) {
-	transformer, _ := NewBarrageRequestTransformer([]string{}, map[string]any{"headerValueRegexp": "log4j.*"})
-	req := http.Request{Header: http.Header{}}
+	transformer, _ := NewBarrageRequestTransformer([]string{}, nil, map[string]any{"headerValueRegexp": "log4j.*"})
+	ux, _ := url.Parse("http://www.example.com")
+	req := http.Request{Header: http.Header{}, URL: ux}
 	wrapper := APIWrapper{Request: &req}
 	_, err := transformer.Transform(&wrapper)
 	if err != nil {
@@ -22,14 +24,14 @@ func TestBarrageRequestTransformer_Transform(t *testing.T) {
 		t.Error("Barrage did not trigger for header value")
 	}
 	req.Header.Del("foo")
-	transformer, _ = NewBarrageRequestTransformer([]string{}, map[string]any{"headerNameRegexp": "Log4j.*"})
+	transformer, _ = NewBarrageRequestTransformer([]string{}, nil, map[string]any{"headerNameRegexp": "Log4j.*"})
 	req.Header.Set("log4jFoo", "123")
 	_, err = transformer.Transform(&wrapper)
 	if err == nil {
 		t.Error("Barrage did not trigger for header name")
 	}
 	req.Header.Del("log4jFoo")
-	transformer, _ = NewBarrageRequestTransformer([]string{}, map[string]any{"headerRegexp": "Foo:log4j.*"})
+	transformer, _ = NewBarrageRequestTransformer([]string{}, nil, map[string]any{"headerRegexp": "Foo:log4j.*"})
 	req.Header.Set("foo", "log4jBananas")
 	_, err = transformer.Transform(&wrapper)
 	if err == nil {
@@ -37,7 +39,7 @@ func TestBarrageRequestTransformer_Transform(t *testing.T) {
 	}
 	req.Body = io.NopCloser(bytes.NewReader([]byte("foo bar foo")))
 	wrapper.ExpandRequest()
-	transformer, _ = NewBarrageRequestTransformer([]string{}, map[string]any{"bodyRegexp": ".*bar.*"})
+	transformer, _ = NewBarrageRequestTransformer([]string{}, nil, map[string]any{"bodyRegexp": ".*bar.*"})
 	_, err = transformer.Transform(&wrapper)
 	if err == nil {
 		t.Error("Barrage did not trigger for body")
--- tests_transformers_delay_test.go
+++ tests_transformers_delay_test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"net/http"
+	"net/url"
 	"testing"
 	"time"
 )
 
 func TestDelayTransformer_Transform(t *testing.T) {
-	transformer, _ := NewDelayTransformer([]string{}, map[string]any{"min": "1s", "max": "3s"})
-	wrapper := APIWrapper{}
+	transformer, _ := NewDelayTransformer([]string{}, nil, map[string]any{"min": "1s", "max": "3s"})
+	ux, _ := url.Parse("http://www.example.com")
+	wrapper := APIWrapper{Request: &http.Request{URL: ux}}
 	before := time.Now()
 	_, _ = transformer.Transform(&wrapper)
 	after := time.Now()
