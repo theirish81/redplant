7fbe4cc7d23b916f59dc284356cfff5f78126b9a
theirish81 <theirish81@gmail.com>
2022-09-17 16:21:41 +0200

    documentation and method refactoring

--- tripper_none.go
+++ tripper_none.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// NoneTrip is a tripper which does... nothing. It will return an empty response with status code 200
 func NoneTrip(request *http.Request) (*http.Response, error) {
 	response := http.Response{StatusCode: 200, Request: request, Uncompressed: true}
 	response.Header = http.Header{}
--- wrappers.go
+++ wrappers.go
@@ -106,7 +106,7 @@ func (w *APIWrapper) ExpandRequest() {
 	if len(w.Request.ExpandedBody) == 0 && w.Request.Body != nil {
 		rawBody, _ := io.ReadAll(w.Request.Body)
 		rawReader := bytes.NewReader(rawBody)
-		if IsGZIP(w.Request.TransferEncoding) {
+		if IsGZIPHeader(w.Request.TransferEncoding) {
 			gzipReader, _ := gzip.NewReader(rawReader)
 			w.Request.ExpandedBody, _ = io.ReadAll(gzipReader)
 		} else {
